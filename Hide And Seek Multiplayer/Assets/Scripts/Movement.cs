7b818fdaf4da204541b74e9072e1fe719bbea0c8
Kamal Johnson <kamal@kraker.in>
2020-11-07 10:19:20 +0530

    :art: Add player transform sync

---
+++ Hide And Seek Multiplayer/Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class Movement : NetworkBehaviour
+{
+
+    public int speed;
+
+    void Update()
+    {
+        if(isLocalPlayer)
+        {
+            var inputX = Input.GetAxis("Horizontal");
+            var inputY = Input.GetAxis("Vertical");
+            transform.position += new Vector3(inputX, 0, inputY) * Time.deltaTime * speed;
+        }
+    }
+}
