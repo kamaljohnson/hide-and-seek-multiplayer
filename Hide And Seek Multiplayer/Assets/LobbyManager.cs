fde02ff309856ea6ef4687850e80afdd158b3d08
Kamal Johnson <kamal@kraker.in>
2020-11-08 05:51:34 +0530

    :art: Add start button in lobby

---
+++ Hide And Seek Multiplayer/Assets/LobbyManager.cs
@@ -0,0 +1,28 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : NetworkBehaviour
+{
+
+    public GameObject startButton;
+
+    private void Start()
+    {
+        if (!isClientOnly)
+        {
+            startButton.SetActive(true);
+        }
+    }
+
+    public void StartGame()
+    {
+        ClientStartGame();
+    }
+
+    [ClientRpc]
+    void ClientStartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
