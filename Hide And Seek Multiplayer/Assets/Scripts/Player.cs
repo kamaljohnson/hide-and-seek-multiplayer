e94f6a57bd5c7cf6bd91e20f567cc111e9132a3d
Kamal Johnson <kamal@kraker.in>
2020-11-08 08:30:50 +0530

    ;art: Add temp player cameral

--- Hide And Seek Multiplayer/Assets/Scripts/Player.cs
+++ Hide And Seek Multiplayer/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Mirror;
+using UnityEngine;
 
 public enum PlayerType
 {
@@ -10,10 +11,16 @@ public class Player : NetworkBehaviour
 {
 
     public PlayerType type;
+    public GameObject camera;
 
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
+        
+        if (!isLocalPlayer)
+        {
+            camera.SetActive(false);
+        }
     }
 
     void Update()
