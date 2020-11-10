885168ac108438f0b97f1b420f5a220f10df10bf
Kamal Johnson <kamal@kraker.in>
2020-11-10 07:02:53 +0530

    :art: Add wall seethrough shader

---
+++ Hide And Seek Multiplayer/Assets/CircleSync.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CircleSync : MonoBehaviour
+{
+
+    public static int PosID = Shader.PropertyToID("_Position");
+    public static int SizeID = Shader.PropertyToID("_Size");
+
+    public Material CurrentMaterial;
+    public Camera Camera;
+
+    public LayerMask Mask;
+
+    void LateUpdate()
+    {
+        var dir = Camera.transform.position - transform.position;
+        var ray = new Ray(transform.position, dir.normalized);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 3000, Mask))
+        {
+            CurrentMaterial = hit.collider.gameObject.GetComponent<Renderer>().sharedMaterial;
+            CurrentMaterial.SetFloat(SizeID, 1);
+        }
+        else
+        {
+            CurrentMaterial.SetFloat(SizeID, 0);
+        }
+
+        var view = Camera.WorldToViewportPoint(transform.position);
+        CurrentMaterial.SetVector(PosID, view);
+    }
+}
